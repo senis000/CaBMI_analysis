b23b72d5802732932246ddb65717caf6ec18dea4
senis000 <nuria.vendrell.llopis@gmail.com>
2019-01-22 11:07:33 -0800

    adding time_lock_activity

--- utils_cabmi.py
+++ utils_cabmi.py
@@ -38,3 +38,15 @@ def sliding_mean(data_array, window=5):
         avg /= float(np.size(indices))
         new_list.append(avg)
     return np.array(new_list)
+
+
+def time_lock_activity (f, t_size=[30,3], tbin=10):
+    # can be optimized by removing f and passing only components needed
+    trial_start = np.asarray(f['trial_start']).astype('int')
+    trial_end = np.asarray(f['trial_end']).astype('int')
+    C = np.asarray(f['C'])
+    neuron_activity = np.ones((trial_end.shape[0], C.shape[0], np.sum(t_size)*tbin))*np.nan
+    for ind, trial in enumerate(trial_end):
+        aux_act = C[:, trial_start[ind]:trial + t_size[1]*tbin]
+        neuron_activity[ind, :, -aux_act.shape[1]:] = aux_act  
+    return neuron_activity   
