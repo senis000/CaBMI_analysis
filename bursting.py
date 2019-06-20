fcbe63086e86d2d662a9abe10c4ab035e70ddd60
albertqu <albert_qu@berkeley.edu>
2019-06-19 22:21:25 -0700

    burstContrast

--- bursting.py
+++ bursting.py
@@ -1,9 +1,10 @@
 import numpy as np
 from scipy.signal import find_peaks
 from scipy import io
-from scipy import stats
-import os
+import os, h5py
 from shuffling_functions import signal_partition
+from plotting_functions import best_nbins
+from utils_loading import get_PTIT_over_days
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
@@ -104,7 +105,7 @@ def neuron_fano_norm(sig, W=None, T=100, lingress=False, pre=True):
         return neuron_fano(sig, W, T) / (n)
 
 
-def deconv_fano_contrast(hIT, hPT, fano_opt='raw'):
+def deconv_fano_contrast_single_pair(hIT, hPT, fano_opt='raw'):
     nneg = True
     W = None
     step = 100
@@ -115,6 +116,7 @@ def deconv_fano_contrast(hIT, hPT, fano_opt='raw'):
         fano_metric = lambda *args: neuron_fano_norm(*args, pre=True)
     else:
         fano_metric = lambda *args: neuron_fano_norm(*args, pre=False)
+
     def get_datas(hfile, data_opt, expr_opt):
         redlabels = np.array(hfile['redlabel'])
         datas = np.array(hfile[data_opt])
@@ -221,4 +223,120 @@ def deconv_fano_contrast(hIT, hPT, fano_opt='raw'):
     step_slider.on_changed(update)
     W_slider.on_changed(update)
     plt.show()
+
+
+def deconv_fano_contrast_avg_days(root, fano_opt='raw', W=None, step=100):
+    all_files = get_PTIT_over_days(root)
+    nneg = True
+    OPT = 'IT VS PT bursting {}'.format(fano_opt)
+    if fano_opt == 'raw':
+        fano_metric = neuron_fano
+    elif fano_opt == 'norm_pre':
+        fano_metric = lambda *args: neuron_fano_norm(*args, pre=True)
+    else:
+        fano_metric = lambda *args: neuron_fano_norm(*args, pre=False)
+
+    def get_datas(hfile, data_opt, expr_opt):
+        redlabels = np.array(hfile['redlabel'])
+        datas = np.array(hfile[data_opt])
+        blen = hfile.attrs['blen']
+        if nneg:
+            datas = datas - np.min(datas, axis=1, keepdims=True)
+        if expr_opt.find('IT') != -1:
+            datas_it = datas[np.logical_and(redlabels, hfile['nerden'])]
+            datas_pt = datas[np.logical_and(~redlabels, hfile['nerden'])]
+        elif expr_opt.find('PT') != -1:
+            datas_it = datas[np.logical_and(~redlabels, hfile['nerden'])]
+            datas_pt = datas[np.logical_and(redlabels, hfile['nerden'])]
+        else:
+            raise RuntimeError('NOT PT OR IT')
+        return {'IT': {'N': datas_it.shape[0], 'data': datas_it,
+                       'base': datas_it[:, :blen], 'online': datas_it[:, blen:]},
+                'PT': {'N': datas_pt.shape[0], 'data': datas_pt,
+                       'base': datas_pt[:, :blen], 'online': datas_pt[:, blen:]}}
+
+    def fano_series(all_data, W, step, day=None, out=None, label=None):
+        datas, datas_base, datas_online, N = all_data['data'], all_data['base'], \
+                                             all_data['online'], all_data['N']
+        nfanos = np.empty(N)
+        base_fanos = np.empty(N)
+        online_fanos = np.empty(N)
+        for j in range(N):
+            fano = fano_metric(datas[j], W, step)
+            fano_base = fano_metric(datas_base[j], W, step)
+            fano_online = fano_metric(datas_online[j], W, step)
+            print(j, fano)
+            nfanos[j] = fano
+            base_fanos[j] = fano_base
+            online_fanos[j] = fano_online
+        if out:
+            if day:
+                if out['nfanos'][day][label] is None:
+                    out['nfanos'][day][label], out['base_fanos'][day][label], \
+                    out['online_fanos'][day][label] = nfanos, base_fanos, online_fanos
+                else:
+                    out['nfanos'][day][label] += nfanos
+                    out['base_fanos'][day][label] += base_fanos
+                    out['online_fanos'][day][label] += online_fanos
+            else:
+                if out['nfanos'][label] is None:
+                    out['nfanos'][label], out['base_fanos'][label], out['online_fanos'][label] = \
+                        nfanos, base_fanos, online_fanos
+                else:
+                    out['nfanos'][label] += nfanos
+                    out['base_fanos'][label] += base_fanos
+                    out['online_fanos'][label] += online_fanos
+        else:
+            return nfanos, base_fanos, online_fanos
+
+    vars = ['IT_expr_IT', 'IT_expr_PT', 'PT_expr_IT', 'PT_expr_PT']
+    labels = ['nfanos', 'base_fanos', 'online_fanos']
+    day_range = range(1, max(len(all_files['IT']), len(all_files['PT']))+1)
+    plot_datas = {label: {i: {d: None for d in vars} for i in day_range} for label in labels}
+
+    for group in 'IT', 'PT':
+        for day in all_files[group]:
+            for expr in all_files[group][day]:
+                hfile = h5py.File(expr, 'r')
+                for celltype in 'IT', 'PT':
+                    data_expr = get_datas(hfile, 'neuron_act', celltype)
+                    var = '{}_expr_{}'.format(group, celltype)
+                    fano_series(data_expr[celltype], W, step, plot_datas, var)
+
+    plt.style.use('bmh')
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    plt.subplots_adjust(bottom=0.3, wspace=0.3, hspace=0.5)
+    fig.suptitle(OPT)
+    for day in day_range:
+        for v in vars:
+            ax[0][0].plot(plot_datas['nfanos'][day][v])
+        ax[0][0].legend(vars)
+        ax[0][0].set_xlabel("Neuron")
+        ax[0][0].set_title("Fano Factor for all neurons")
+
+        all_stats = {l: {d: {v: {} for v in vars} for d in day_range} for l in labels}
+        all_stats['meta'] = {'W': W if W else -1, 'T': step}
+        for i, label in enumerate(labels):
+            curr = i + 1
+            stat = ['NA'] * 12
+            r, c = curr // 2, curr % 2
+            legs = []
+            for j, v in enumerate(vars):
+                fanos = plot_datas[label][day][v]
+                # Choice of bin size: Ref: https://www.fmrib.ox.ac.uk/datasets/techrep/tr00mj2/tr00mj2/node24
+                # .html
+                if fanos:
+                    miu, sigma, N = np.around(np.nanmean(fanos), 5), np.around(np.nanstd(fanos), 5), len(fanos)
+                    nbins = best_nbins(fanos)
+                    ax[r][c].hist(fanos, bins=int((max(fanos) - min(fanos)) / nbins + 1), density=True, alpha=0.6)
+                    stat[j], stat[j + 4], stat[j + 8] = miu, sigma, N
+                    all_stats[label][day][v]['mean'] = stat[j]
+                    all_stats[label][day][v]['std'] = stat[j + 4]
+                    all_stats[label][day][v]['N'] = stat[j + 8]
+                    legs.append(v)
+            ax[r][c].legend(legs)
+            ax[r][c].set_title("{}".format(label), fontsize=10)
+        outpath = "/Users/albertqu/Documents/7.Research/BMI/analysis_data/bursty_log"
+        io.savemat(os.path.join(outpath, 'fano_{}_stats_{}.mat'.format(fano_opt, all_stats['meta'])))
+
 
---
+++ utils_loading.py
@@ -0,0 +1,21 @@
+import os
+
+
+def get_PTIT_over_days(root):
+    results = {'IT': {}, 'PT': {}}
+    for animal in os.listdir(root):
+        if animal.find('IT') == -1 and animal.find('PT') == -1:
+            continue
+        animal_path = os.path.join(root, animal)
+        for i, day in enumerate(os.listdir(animal_path)):
+            j = i+1
+            if day.find('IT') == -1 and day.find('PT') == -1:
+                continue
+            group = animal[:2]
+            file = os.path.join(animal_path, day)
+            if j in results[group]:
+                results[group][j].append(file)
+            else:
+                results[group][j] = [file]
+    return results
+
