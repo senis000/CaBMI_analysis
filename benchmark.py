7d0851f94b0ada2db345ef14d4b0184a65d6e390
Ching Fang <chingfang17@berkeley.edu>
2019-06-26 08:05:50 -0700

    log_benchmark

--- benchmark.py
+++ benchmark.py
@@ -129,14 +129,22 @@ def plot_calcium_dist_spikefinder(outpath=None, W=None, T=100, eps=True):
     calcium_s = np.concatenate([measures['calcium_std'][i]for i in measures['calcium_std']])
     idsort = np.argsort(all_spikes)
     sorted_spikes, sorted_cm, sorted_cs = all_spikes[idsort], calcium_m[idsort], calcium_s[idsort]
-    fig = plt.figure(figsize=(20,10))
-    plt.plot(sorted_spikes, sorted_cm)
-    plt.fill_between(sorted_spikes, sorted_cm+sorted_cs, sorted_cm-sorted_cs, color='#089FFF', alpha=0.2)
+    sorted_varratio = sorted_cs ** 2 / sorted_cm ** 2 + 1
+    corrm = np.corrcoef(sorted_spikes, sorted_cm)
+    corrs = np.corrcoef(sorted_spikes, sorted_cs)
+    corrv = np.corrcoef(sorted_spikes, sorted_varratio)
+    fig, axes = plt.figure(nrows=2, ncols=1, figsize=(20,10))
+    axes[0].plot(sorted_spikes, sorted_cm)
+    axes[0].fill_between(sorted_spikes, sorted_cm+sorted_cs, sorted_cm-sorted_cs, color='#089FFF', alpha=0.2)
+    axes[1].plot(sorted_spikes, sorted_varratio)
+    axes[1].set_title('Moment Ratio')
+    fig.suptitle("Corr: mean: {}, std: {}, moment: {}".format(corrm, corrs, corrv))
     iname = os.path.join(outpath, 'fano_calcium_trend_W{}_T{}'.format(W, T))
     fig.savefig(iname+'.png')
     if eps:
         fig.savefig(iname+'.eps')
     plt.close('all')
+    measures['meta'] = {'corr_mean': corrm, 'corr_std': corrs, 'corr_moment_ratio': corrv}
     io.savemat(os.path.join(root, 'datalog', "calcium_distribution_W{}_T{}".format(W, T) + '.mat'), measures)
     return measures
 
@@ -186,7 +194,7 @@ def test_calcium_dist():
     W = None
     binT = 10
     sampled = False
-    for T in [10, 1, 20, 50]:
+    for T in [10, 1, 20, 50, 100]:
         for fano, p in list(itertools.product(['norm_pre', 'raw', 'norm_post'], [1, 2])):
             print('opt:', fano, p)
             saveopt = 'deconvFano_T{}_p{}_{}_{}'.format(T, p, fano, source_name)
