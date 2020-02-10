0c35966228b0162204d0e38c29585d6c496d9a27
Ching Fang <chingfang17@berkeley.edu>
2020-02-10 12:01:41 -0800

    log chekc

--- caiman_test.py
+++ caiman_test.py
@@ -176,6 +176,207 @@ def dff_sanity_check(rawbase, processed, nproc=1, group='*', out=None, csvout=No
             csvf.close()
 
 
+def single_dff_nan_test():
+    from pipeline import *
+    folder ="/media/user/Seagate Backup Plus Drive/Nuria_data/CaBMI/Layer_project/"
+    animal = 'IT5'
+    day = '190129'
+    folder_path = folder + 'raw/' + animal + '/' + day + '/'
+    folder_final = folder + 'processed/' + animal + '/' + day + '/'
+    err_file = open(folder_path + "errlog.txt", 'a+')  # ERROR HANDLING
+    if not os.path.exists(folder_final):
+        os.makedirs(folder_final)
+    
+    finfo = folder_path +  'wmat.mat'  #file name of the mat 
+    matinfo = scipy.io.loadmat(finfo)
+    ffull = [folder_path + matinfo['fname'][0]]            # filename to be processed
+    fbase = [folder_path + matinfo['fbase'][0]]
+    number_planes=4
+    number_planes_total=6
+    num_files, len_bmi = separate_planes(folder, animal, day, ffull, 'bmi', number_planes, number_planes_total)
+    num_files_b, len_base = separate_planes(folder, animal, day, fbase, 'baseline', number_planes, number_planes_total)
+    dend=False; display_images=True
+    folder_path = folder + 'raw/' + animal + '/' + day + '/separated/'
+    finfo = folder + 'raw/' + animal + '/' + day + '/wmat.mat'  #file name of the mat 
+    matinfo = scipy.io.loadmat(finfo)
+    initialZ = int(matinfo['initialZ'][0][0])
+    fr = matinfo['fr'][0][0]
+    
+    if dend:
+        sec_var = 'Dend'
+    else:
+        sec_var = ''
+    
+    print('*************Starting with analysis*************')
+    neuron_mats = []
+    plane = 1
+    dff_all = []
+    neuron_act_all = []
+    fnames = []
+    for nf in np.arange(int(num_files_b)):
+        fnames.append(folder_path + 'baseline' + '_plane_' + str(plane) + '_nf_' + str(nf) + '.tiff')
+    print('performing plane: ' + str(plane))
+    for nf in np.arange(int(num_files)):
+        fnames.append(folder_path + 'bmi' + '_plane_' + str(plane) + '_nf_' + str(nf) + '.tiff')
+        
+    fpath = folder + 'raw/' + animal + '/' + day + '/analysis/' + str(plane) + '/'
+    if not os.path.exists(fpath):
+        os.makedirs(fpath)
+        
+    try:
+        f = h5py.File(folder + 'raw/' + animal + '/' + day + '/' + 'bmi_' + sec_var + '_' + str(plane) + '.hdf5', 'w-')
+    except IOError:
+        print(" OOPS!: The file already existed ease try with another file, new results will NOT be saved")
+        continue 
+        
+    zval = calculate_zvalues(folder, plane)
+    print(fnames)
+    z = zval
+
+    decay_time = 0.4                    # length of a typical transient in seconds    
+
+    # Look for the best parameters for this 2p system and never change them again :)
+    # motion correction parameters
+    niter_rig = 1               # number of iterations for rigid motion correction
+    max_shifts = (3, 3)         # maximum allow rigid shift
+    splits_rig = 10            # for parallelization split the movies in  num_splits chuncks across time
+    strides = (96, 96)          # start a new patch for pw-rigid motion correction every x pixels
+    overlaps = (48, 48)         # overlap between pathes (size of patch strides+overlaps)
+    splits_els = 10             # for parallelization split the movies in  num_splits chuncks across time
+    upsample_factor_grid = 4    # upsample factor to avoid smearing when merging patches
+    max_deviation_rigid = 3     # maximum deviation allowed for patch with respect to rigid shifts
+    
+    # parameters for source extraction and deconvolution
+    p = 1                       # order of the autoregressive system
+    gnb = 2                     # number of global background components
+    merge_thresh = 0.8          # merging threshold, max correlation allowed
+    rf = 25                     # half-size of the patches in pixels. e.g., if rf=25, patches are 50x50
+    stride_cnmf = 10            # amount of overlap between the patches in pixels
+    K = 25                       # number of components per patch
+    
+    if dend:
+        gSig = [1, 1]               # expected half size of neurons
+        init_method = 'sparse_nmf'  # initialization method (if analyzing dendritic data using 'sparse_nmf')
+        alpha_snmf =  1e-6          # sparsity penalty for dendritic data analysis through sparse NMF
+    else:
+        gSig = [3, 3]               # expected half size of neurons
+        init_method = 'greedy_roi'  # initialization method (if analyzing dendritic data using 'sparse_nmf')
+        alpha_snmf = None           # sparsity penalty for dendritic data analysis through sparse NMF
+    
+    # parameters for component evaluation
+    min_SNR = 2.5               # signal to noise ratio for accepting a component
+    rval_thr = 0.8              # space correlation threshold for accepting a component
+    cnn_thr = 0.8               # threshold for CNN based classifier
+
+    dview = None # parallel processing keeps crashing. 
+    
+    print('***************Starting motion correction*************')
+    print('files:')
+    print(fnames)
+    
+    
+    # %% start a cluster for parallel processing
+    #c, dview, n_processes = cm.cluster.setup_cluster(backend='local', n_processes=None, single_thread=False)
+    
+    #%%% MOTION CORRECTION
+    # first we create a motion correction object with the parameters specified
+    min_mov = cm.load(fnames[0]).min()
+    # this will be subtracted from the movie to make it non-negative
+    
+    mc = MotionCorrect(fnames, min_mov,
+                       dview=dview, max_shifts=max_shifts, niter_rig=niter_rig,
+                       splits_rig=splits_rig,
+                       strides=strides, overlaps=overlaps, splits_els=splits_els,
+                       upsample_factor_grid=upsample_factor_grid,
+                       max_deviation_rigid=max_deviation_rigid,
+                       shifts_opencv=True, nonneg_movie=True)
+    # note that the file is not loaded in memory
+    
+    #%% Run piecewise-rigid motion correction using NoRMCorre
+    mc.motion_correct_pwrigid(save_movie=True)
+    bord_px_els = np.ceil(np.maximum(np.max(np.abs(mc.x_shifts_els)),
+                                     np.max(np.abs(mc.y_shifts_els)))).astype(np.int)
+                            
+    totdes = [np.nansum(mc.x_shifts_els), np.nansum(mc.y_shifts_els)]
+    print('***************Motion correction has ended*************')
+    # maximum shift to be used for trimming against NaNs
+
+    #%% MEMORY MAPPING
+    # memory map the file in order 'C'
+    fnames = mc.fname_tot_els   # name of the pw-rigidly corrected file.
+    fname_new = cm.save_memmap(fnames, base_name='memmap_', order='C', border_to_0=bord_px_els)  # exclude borders
+    
+    # now load the file
+    Yr, dims, T = cm.load_memmap(fname_new)
+    d1, d2 = dims
+    images = np.reshape(Yr.T, [T] + list(dims), order='F')
+    # load frames in python format (T x X x Y)
+    
+    
+    # %% restart cluster to clean up memory
+    #cm.stop_server(dview=dview)
+    #c, dview, n_processes = cm.cluster.setup_cluster(backend='local', n_processes=None, single_thread=False)
+        
+    
+    #%% RUN CNMF ON PATCHES
+    print('***************Running CNMF...*************')
+    
+    # First extract spatial and temporal components on patches and combine them
+    # for this step deconvolution is turned off (p=0)
+    
+    cnm = cnmf.CNMF(n_processes=1, k=K, gSig=gSig, merge_thresh=merge_thresh,
+                    p=0, dview=dview, rf=rf, stride=stride_cnmf, memory_fact=1,
+                    method_init=init_method, alpha_snmf=alpha_snmf,
+                    only_init_patch=False, gnb=gnb, border_pix=bord_px_els)
+    cnm = cnm.fit(images)
+    
+    
+    #%% COMPONENT EVALUATION
+    # the components are evaluated in three ways:
+    #   a) the shape of each component must be correlated with the data
+    #   b) a minimum peak SNR is required over the length of a transient
+    #   c) each shape passes a CNN based classifier
+    
+    idx_components, idx_components_bad, SNR_comp, r_values, cnn_preds = \
+        estimate_components_quality_auto(images, cnm.estimates.A, cnm.estimates.C, cnm.estimates.b, cnm.estimates.f,
+                                         cnm.estimates.YrA, fr, decay_time, gSig, dims,
+                                         dview=dview, min_SNR=min_SNR,
+                                         r_values_min=rval_thr, use_cnn=False,
+                                         thresh_cnn_min=cnn_thr)
+    
+     
+    if display_images:
+        plt.figure()
+        plt.subplot(131)
+
+        auxb = np.transpose(np.reshape(cnm.estimates.b[:,0], [int(np.sqrt(cnm.estimates.b.shape[0])), int(np.sqrt(cnm.estimates.b.shape[0]))]))
+        plt.imshow(auxb)
+        plt.title('Raw mean')
+        plt.subplot(132)
+        crd_good = cm.utils.visualization.plot_contours(
+            cnm.estimates.A[:, idx_components], auxb, thr=.8)
+        plt.title('Contour plots of accepted components')
+        plt.subplot(133)
+        crd_bad = cm.utils.visualization.plot_contours(
+            cnm.estimates.A[:, idx_components_bad], auxb, thr=.8, vmax=0.2)
+        plt.title('Contour plots of rejected components')
+        plt.savefig(fpath + 'comp.png', bbox_inches="tight")
+         
+        plt.close('all')
+    
+    
+    #%% RE-RUN seeded CNMF on accepted patches to refine and perform deconvolution
+    A_in, C_in, b_in, f_in = cnm.estimates.A[:, idx_components], cnm.estimates.C[idx_components], cnm.estimates.b, cnm.estimates.f
+    cnm2 = cnmf.CNMF(n_processes=1, k=A_in.shape[-1], gSig=gSig, p=p, dview=dview,
+                     merge_thresh=merge_thresh, Ain=A_in, Cin=C_in, b_in=b_in,
+                     f_in=f_in, rf=None, stride=None, gnb=gnb,
+                     method_deconvolution='oasis', check_nan=True)
+    
+    print('***************Fit*************')
+    cnm2 = cnm2.fit(images)
+    F_dff = detrend_df_f(cnm2.estimates.A, cnm2.estimates.b, cnm2.estimates.C, cnm2.estimates.f, YrA=cnm2.estimates.YrA, quantileMin=8, frames_window=250)
+
+
 if __name__ == '__main__':
     root = '/run/user/1000/gvfs/smb-share:server=typhos.local,share=data_01/NL/layerproject/'
     rawbase = os.path.join(root, 'raw')
